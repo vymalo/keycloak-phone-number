b7f787f18ec4796e1ba81120cdb352a57fe20476
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-03-09 11:44:48 +0100

    feat: refactor SmsService to use OAuth2 Client Credentials Grant instead of Basic Auth

--- src/main/java/com/vymalo/keycloak/services/SmsService.java
+++ src/main/java/com/vymalo/keycloak/services/SmsService.java
@@ -10,12 +10,23 @@ import com.vymalo.api.server.model.ConfirmTanRequest;
 import com.vymalo.api.server.model.SendSmsRequest;
 import com.vymalo.keycloak.constants.ConfigKey;
 import com.vymalo.keycloak.constants.Utils;
-import lombok.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.jbosslog.JBossLog;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.util.JsonSerialization;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -23,26 +34,31 @@ import java.util.stream.Collectors;
 @JBossLog
 public class SmsService {
     public static final String DEFAULT_PHONE_KEY_NAME = "phoneNumber";
-    public static final PhoneNumberUtil phoneNumberUtil = com.google.i18n.phonenumbers.PhoneNumberUtil.getInstance();
+    public static final PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
     @Getter
     private static final Set<CountryPhoneCode> allCountries;
 
     @Getter
     private static final SmsService instance;
 
+    private static String accessToken;
+    private static long tokenExpirationTime; // Timestamp (millis) when token expires
+
     static {
         final var smsUrl = Utils.getEnv(ConfigKey.CONF_PRP_SMS_URL);
-        final var basicUsr = Utils.getEnv(ConfigKey.BASIC_AUTH_USERNAME);
-        final var basicPwd = Utils.getEnv(ConfigKey.BASIC_AUTH_PASSWORD);
+        final var clientId = Utils.getEnv("OAUTH2_CLIENT_ID");
+        final var clientSecret = Utils.getEnv("OAUTH2_CLIENT_SECRET");
+        final var tokenEndpoint = Utils.getEnv("OAUTH2_TOKEN_ENDPOINT");
         final var allowedCountries = Pattern.compile(Utils.getEnv(ConfigKey.ALLOWED_COUNTRY_PATTERN, "*"), Pattern.CASE_INSENSITIVE).asMatchPredicate();
 
         final var apiClient = new ApiClient();
         apiClient.updateBaseUri(smsUrl);
         apiClient.setRequestInterceptor(builder -> {
-            if (StringUtils.isNotEmpty(basicUsr) && StringUtils.isNotEmpty(basicPwd)) {
-                final var valueToEncode = basicUsr + ":" + basicPwd;
-                final var basicAuth = "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
-                builder.header("Authorization", basicAuth);
+            try {
+                String token = getAccessToken(clientId, clientSecret, tokenEndpoint);
+                builder.header("Authorization", "Bearer " + token);
+            } catch (Exception e) {
+                log.error("Failed to set OAuth2 token", e);
             }
         });
 
@@ -72,6 +88,33 @@ public class SmsService {
 
     private SmsService(ApiClient apiClient) {
         this.smsApi = new SmsApi(apiClient);
+    }
+
+    // Fetch or refresh OAuth2 access token
+    private static synchronized String getAccessToken(String clientId, String clientSecret, String tokenEndpoint) throws IOException {
+        if (accessToken == null || System.currentTimeMillis() >= tokenExpirationTime) {
+            try (CloseableHttpClient client = HttpClients.createDefault()) {
+                HttpPost post = new HttpPost(tokenEndpoint);
+                post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("grant_type", "client_credentials"));
+                params.add(new BasicNameValuePair("client_id", clientId));
+                params.add(new BasicNameValuePair("client_secret", clientSecret));
+                post.setEntity(new UrlEncodedFormEntity(params));
+
+                try (CloseableHttpResponse response = client.execute(post)) {
+                    String json = EntityUtils.toString(response.getEntity());
+                    Map<String, Object> tokenData = JsonSerialization.readValue(json, Map.class);
+                    accessToken = (String) tokenData.get("access_token");
+                    int expiresIn = (int) tokenData.get("expires_in");
+                    tokenExpirationTime = System.currentTimeMillis() + (expiresIn * 1000L) - 30000;
+                }
+            } catch (Exception e) {
+                throw new IOException("Failed to fetch OAuth2 token", e);
+            }
+        }
+        return accessToken;
     }
 
     public Optional<String> sendSmsAndGetHash(String phoneNumber) {
@@ -106,7 +149,6 @@ public class SmsService {
             final var firstPhoneNumber = phoneNumber.stream().findFirst().flatMap(this::parse);
             return firstPhoneNumber.isPresent();
         }
-
         return true;
     }
 
